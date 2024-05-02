69e09b645fbd634ebf265f584e0b1912cde17f0c
Tacy Prins Woodlief <48765699+TacyPW@users.noreply.github.com>
2024-05-02 15:42:15 -0700

    chapters 1-3 of tutorial

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Alegreya_Sans } from "next/font/google";
+
+export const alegreyaSans = Alegreya_Sans({ weight: ['400'], subsets: ['latin'] });
+
+import { Lusitana  } from "next/font/google";
+
+export const lusitana = Lusitana({ weight: ['400'], subsets: ['latin'] });
